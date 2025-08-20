b52b0f815629c2ed9e02289aeab710426db1f840
prabdayoon405 <prabdayoon405@gmail.com>
2025-08-20 19:40:50 +0530

    Add files via upload

---
+++ js/script2.js
@@ -0,0 +1,17 @@
+
+
+  $(document).ready(function() {
+   var audioElement = document.createElement('audio');
+   audioElement.setAttribute('src', 'media/jp.mp4');
+
+   audioElement.addEventListener('ended', function() {
+       this.play();
+   }, false);
+
+
+    $('#mycanvas').click(function() {
+       audioElement.play();
+
+   });
+
+});
